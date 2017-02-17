dfcb17478412b8cbb160a2193de8c8f210f32db4
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-16 20:09:48 -0500

    feat: Setting the scene
    includes steamVR plugin

---
+++ Assets/Scripts/SetPhysicsPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPhysicsPosition : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public SteamVR_TrackedObject controller;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        rb.MovePosition(controller.transform.position);
+        rb.MoveRotation(controller.transform.rotation);
+	}
+}
