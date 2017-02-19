427a45a565e2ac2731ae0559e0cdad8118564324
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-19 14:37:28 -0500

    feat: magnus effect
    added magnus effect for ball rotation

---
+++ Assets/Scripts/MagnusEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnusEffect : MonoBehaviour {
+
+    public float magnusConst = 0.005f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        Vector3 force = magnusConst * Vector3.Cross(rb.velocity, rb.angularVelocity);
+        rb.AddRelativeForce(force);
+	}
+}
